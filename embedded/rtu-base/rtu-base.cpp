4d6f2ed6aca201035622a59647a36d1813b0bf8e
mjkoster <michaeljohnkoster@gmail.com>
2022-03-07 17:52:29 -0800

    Update rtu-base.cpp

--- embedded/rtu-base/rtu-base.cpp
+++ embedded/rtu-base/rtu-base.cpp
@@ -1,42 +1,42 @@
 /* rtu-base contains the base types and fundamental application types */
+#include <stdint.h> 
 
 
 /* global common types */
 
 struct instancelink {
-  uint16_t typeID,
-  uint16_t instanceID
-}
+  uint16_t typeID;
+  uint16_t instanceID;
+};
 
 /* base classes */
 
-class onefbItem {
+class Item {
   public:
-    uint16_t typeID = 65535 ;
+    uint16_t typeID = 65535;
     uint16_t instanceID = 0;
-    string description;
+    char* description;
     void initialize();
-}
+};
 
-class Resource: public onefbItem {
+class Resource: public Item {
   public:
     union value {
-      bool booleantype,
-      int integertype,
-      float floattype,
-      string* stringptype,
-      instancelink linktype
-    enum valueType { boolean, integer, float, string, link };
-    bool multi_instance = false; // multi-instance capable
+      bool booleantype;
+      int integertype;
+      float floattype;
+      char** stringptype;
+      instancelink linktype;
     };
-
+    enum valueType { boolean_type, integer_type, float_type, string_type, link_type };
+    bool multi_instance = 0; // multi-instance capable
 };
-class Object: public onefbItem {
+class Object: public Item {
   public:
-    Resource* resource_list[];
     void syncTime() {};
     void syncOutput() {};
     void syncInput() {};
+    Resource* resource_list[10];
 };
 
 
@@ -44,31 +44,31 @@ class Object: public onefbItem {
 
 class inputLink: public Resource {
   public:
-    typeID = 9100; // class variable?
-    description = "Input Link pointing to an object to retrieve a value from"
+    int typeID = 9100; // class variable?
+    char* description = "Input Link pointing to an object to retrieve a value from";
     instancelink value;
 };
 
 class outputLink: public Resource {
   public:
-    typeID = 9101; // class variable?
-    description = "Output Link pointing to an object to apply a value to"
+    int typeID = 9101; // class variable?
+    char* description = "Output Link pointing to an object to apply a value to";
     instancelink value;
-    multi_instance = true;
+    bool multi_instance = true;
 };
 
 class embeddedNode: public Object {
   public:
-    typeID = 9200;
-    description = "The node object contains a list of object instances and other node-global information"
+    int typeID = 9200;
+    char* description = "The node object contains a list of object instances and other node-global information";
     Object* object_list[];
 };
 
 class schedulertimer: public Object {
   public:
-    typeID = 9201;
-    description = "Timer object";
-    void updateTimer(time_t msec) {};
+    int typeID = 9201;
+    char* description = "Timer object";
+    void updateTimer(uint32_t msec) {};
     int timer_interval;
     outputLink* output_link[];
 };
