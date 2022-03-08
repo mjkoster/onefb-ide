da83b29c8060c474635056bc578e5ba0fc2255b4
mjkoster <michaeljohnkoster@gmail.com>
2022-03-07 18:40:43 -0800

    Update rtu-base.cpp

--- embedded/rtu-base/rtu-base.cpp
+++ embedded/rtu-base/rtu-base.cpp
@@ -1,8 +1,10 @@
 /* rtu-base contains the base types and fundamental application types */
 #include <stdint.h> 
-
-
+#include <string>
+#include <vector>
 /* global common types */
+
+using namespace std;
 
 struct instancelink {
   uint16_t typeID;
@@ -15,18 +17,18 @@ class Item {
   public:
     uint16_t typeID = 65535;
     uint16_t instanceID = 0;
-    char* description;
-    void initialize();
+    string description;
+    void initialize() {};
 };
 
 class Resource: public Item {
   public:
     union value {
-      bool booleantype;
-      int integertype;
-      float floattype;
-      char** stringptype;
-      instancelink linktype;
+      bool boolean_type;
+      int integer_type;
+      float float_type;
+      string* string_type;
+      instancelink link_type;
     };
     enum valueType { boolean_type, integer_type, float_type, string_type, link_type };
     bool multi_instance = 0; // multi-instance capable
@@ -37,38 +39,38 @@ class Object: public Item {
     void syncOutput() {};
     void syncInput() {};
     Resource* resource_list[10];
+    vector<Resource*> resources;
 };
-
 
 /* base application type definitions */
 
 class inputLink: public Resource {
   public:
-    int typeID = 9100; // class variable?
-    char* description = "Input Link pointing to an object to retrieve a value from";
+    uint16_t typeID = 9100; // class variable?
+    string description = "Input Link pointing to an object to retrieve a value from";
     instancelink value;
 };
 
 class outputLink: public Resource {
   public:
-    int typeID = 9101; // class variable?
-    char* description = "Output Link pointing to an object to apply a value to";
+    uint16_t typeID = 9101; // class variable?
+    string description = "Output Link pointing to an object to apply a value to";
     instancelink value;
     bool multi_instance = true;
 };
 
 class embeddedNode: public Object {
   public:
-    int typeID = 9200;
-    char* description = "The node object contains a list of object instances and other node-global information";
+    uint16_t typeID = 9200;
+    string description = "The node object contains a list of object instances and other node-global information";
     Object* object_list[];
 };
 
 class schedulertimer: public Object {
   public:
-    int typeID = 9201;
-    char* description = "Timer object";
+    uint16_t typeID = 9201;
+    string description = "Timer object";
     void updateTimer(uint32_t msec) {};
-    int timer_interval;
+    uint32_t timer_interval;
     outputLink* output_link[];
 };
