1a0455566f43db08b2969792fcf0e1fbf50717c0
mjkoster <michaeljohnkoster@gmail.com>
2022-03-08 18:23:51 -0800

    Update rtu-base-obj.cpp

--- embedded/rtu-base/rtu-base-obj.cpp
+++ embedded/rtu-base/rtu-base-obj.cpp
@@ -1,52 +1,56 @@
 /* rtu-base contains the base types and fundamental application types */
+
 #include <stdint.h> 
-#include <string>
+
 /* global common types */
 
-using namespace std;
-
-struct instancelink {
+struct InstanceLink {
   uint16_t typeID;
   uint16_t instanceID;
 };
 
-union value {
-  bool boolean_type;
-  int integer_type;
-  float float_type;
-  string* string_type;
-  instancelink link_type;
+union AnyValueType {
+  bool booleanType;
+  int integerType;
+  float floatType;
+  char* stringType;
+  InstanceLink linkType;
 };
 
 /* base classes */
 
-class Item {
+/* Resources expose values */
+class Resource {
   public:
     uint16_t typeID;
-    uint16_t instanceID;
-    string description;
-    void initialize() {};
-};
-
-/* Resources expose values */
-class Resource: public Item {
-  public:
-    value resourceValue;
-    enum valueType { boolean_type, integer_type, float_type, string_type, link_type };
+    uint16_t instanceID;    
+    AnyValueType value;
+    enum valueType { booleanType, integerType, floatType, stringType, linkType };
 };
 
 /* Objects contain a collection of resources and some bound methods */
 /* Bound methods are extended for functional types */
-class Object: public Item {
+class Object {
   public:
-    // External Interface
+    uint16_t typeID;
+    uint16_t instanceID;
+    void init() {};
+    
+    // External Interface, standard implementation
     void timerUpdate() {}; // Update Timer on Object
     void inputUpdate() {}; // Fetch Value from Object
-    void outputUpdate() {}; // Update Value on Object
-    // Internal Interface
+    void outputUpdate() {}; // Update Value on Object 
+
+    // Internal Interface, implements application logic
     void onInterval() {}; // Handler for Timer interval
-    void onUpdate(value incomingValue) {}; // Handler for Value update 
-    value onInput() {}; // Handler for Value fetch
-    Resource* resourceList[];
+    void onUpdate(AnyValueType incomingValue) {}; // Handler for Value update 
+    AnyValueType onInput() {
+      AnyValueType returnValue;
+      returnValue.integerType = 0;
+      return returnValue;
+    }; // Handler for Value fetch
+
+    // Resources that are part of this object
+    Resource resource[];
 };
 
