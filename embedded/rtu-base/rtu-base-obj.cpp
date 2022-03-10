98eb36ede33c9f2dc900837e076558e4da2f8aab
mjkoster <michaeljohnkoster@gmail.com>
2022-03-09 21:32:32 -0800

    Update rtu-base-obj.cpp
    literal to initialize the anyvalue union

--- embedded/rtu-base/rtu-base-obj.cpp
+++ embedded/rtu-base/rtu-base-obj.cpp
@@ -241,21 +241,16 @@ class ObjectList {
 
 int main() {
   ObjectList rtu;
-  AnyValueType value;
   Object* object = rtu.newObject(9999,1);
   object -> newResource(1111, 1, integerType);
   object -> newResource(2222, 2, floatType);
-  value.integerType = 100;
-  object -> updateValueByID(1111, 1, value);
-  value.floatType = 101.1;
-  object -> updateValueByID(2222, 2, value);
+  object -> updateValueByID(1111, 1, (AnyValueType){.integerType=100});
+  object -> updateValueByID(2222, 2, (AnyValueType){.floatType=101.1});
   object = rtu.newObject(9090,2);
   object -> newResource(1010, 1, integerType);
   object -> newResource(1010, 2, floatType);
-  value.integerType = 1001;
-  object -> updateValueByID(1010, 1, value);
-  value.floatType = 1001.01;
-  object -> updateValueByID(1010, 2, value);
+  object -> updateValueByID(1010, 1, (AnyValueType){.integerType=1001});
+  object -> updateValueByID(1010, 2, (AnyValueType){.floatType=1001.01});
 
   rtu.displayObjects();
   return(0);
