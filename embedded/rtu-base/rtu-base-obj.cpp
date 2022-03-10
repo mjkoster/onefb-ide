e01edbde56450d854f225c95469902a9708fd428
mjkoster <michaeljohnkoster@gmail.com>
2022-03-09 22:03:56 -0800

    Update rtu-base-obj.cpp
    Code for readDefault and updateDefault

--- embedded/rtu-base/rtu-base-obj.cpp
+++ embedded/rtu-base/rtu-base-obj.cpp
@@ -90,6 +90,23 @@ class Object {
       // 1. InputValue, 2. CurrentValue, 3. OutputValue
       // if (upadteValueByID (InputValueType,0,value) != NULL) {return}
       // etc.
+      Resource* resource = getResourceByID(InputValueType,0);
+      if (resource != NULL) {
+        resource -> value = value;
+        return;
+      };
+      resource = getResourceByID(CurrentValueType,0);
+      if (resource != NULL) {
+        resource -> value = value;
+        return;
+      };
+      resource = getResourceByID(OutputValueType,0);
+      if (resource != NULL){
+        resource -> value = value;
+        return;
+      };
+      printf("updateDefault couldn't find a candidate resource\n");
+      return;
     }; 
     void updateValueByID(uint16_t type, uint16_t instance, AnyValueType value) {
       Resource* resource = getResourceByID(type, instance);
@@ -106,8 +123,20 @@ class Object {
       AnyValueType returnValue;
       // if (returnValue = readValueByID(OutputValueType,0) != NULL)
       // etc.
-      returnValue.integerType = 0; // 1. OutputValue, 2. CurrentValue, 3. InputValue
-      return returnValue;
+      Resource* resource = getResourceByID(OutputValueType,0);
+      if (resource != NULL) {
+        return(resource -> value);
+      };
+      resource = getResourceByID(CurrentValueType,0);
+      if (resource != NULL) {
+        return(resource -> value);
+      };
+      resource = getResourceByID(InputValueType,0);
+      if (resource != NULL){
+        return(resource -> value);
+      };
+      printf("readDefault couldn't find a candidate resource\n");
+      return(returnValue);
     }; 
     AnyValueType readValueByID(uint16_t type, uint16_t instance) {
       Resource* resource = getResourceByID(type, instance);
@@ -243,13 +272,13 @@ int main() {
   ObjectList rtu;
   Object* object = rtu.newObject(9999,1);
   object -> newResource(1111, 1, integerType);
-  object -> newResource(2222, 2, floatType);
   object -> updateValueByID(1111, 1, (AnyValueType){.integerType=100});
+  object -> newResource(2222, 2, floatType);
   object -> updateValueByID(2222, 2, (AnyValueType){.floatType=101.1});
   object = rtu.newObject(9090,2);
   object -> newResource(1010, 1, integerType);
-  object -> newResource(1010, 2, floatType);
   object -> updateValueByID(1010, 1, (AnyValueType){.integerType=1001});
+  object -> newResource(1010, 2, floatType);
   object -> updateValueByID(1010, 2, (AnyValueType){.floatType=1001.01});
 
   rtu.displayObjects();
